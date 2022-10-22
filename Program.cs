f97fe7869a35ecf113f8d19e8b79f7309b3f1f05
Olga <perepelka1989@gmail.com>
2022-10-22 20:00:52 +0300

    add max

--- Program.cs
+++ Program.cs
@@ -12,10 +12,12 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number1>number2)
 {
+       Console.Write("max=");
        Console.WriteLine(number1);
 }
 else
 {
+    Console.Write("max=");
     Console.WriteLine(number2);
 }
 
