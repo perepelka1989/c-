dc8abfba28b7e6ee069d66a66fcb05c4866ff524
Olga <perepelka1989@gmail.com>
2022-10-23 13:58:35 +0300

    dz3

--- Program.cs
+++ Program.cs
@@ -19,31 +19,52 @@
 //     Console.WriteLine("второе число максимальное");
 // }
 
+
+
 //  Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Введите число 1");
- int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 1");
+//  int number1 = Convert.ToInt32(Console.ReadLine());
 
- Console.WriteLine("Введите число 2");
- int number2 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите число 2");
+//  int number2 = Convert.ToInt32(Console.ReadLine());
 
- Console.WriteLine("Введите число 3");
- int number3 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите число 3");
+//  int number3 = Convert.ToInt32(Console.ReadLine());
 
- int max= number1;
- if (number2>max)
+//  int max= number1;
+//  if (number2>max)
+//  {
+//     Console.WriteLine(max= number2);
+//  }
+//  else
+//  {
+//     Console.WriteLine(max= number1);
+//  }
+//  if (number3>max)
+//  {
+//     Console.WriteLine(max= number3);
+//  }
+ 
+
+
+//   Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//   является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number%2==0)
  {
-    Console.WriteLine(max= number2);
+    Console.WriteLine("число четное");
  }
  else
  {
-    Console.WriteLine(max= number1);
+    Console.WriteLine("число нечетное");
  }
- if (number3>max)
- {
-    Console.WriteLine(max= number3);
- }
- 
