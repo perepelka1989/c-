23465c9aef028688944948d62cae73a031c059b0
Olga <perepelka1989@gmail.com>
2022-11-16 14:21:13 +0300

    Add dz5

--- Program.cs
+++ Program.cs
@@ -1,51 +1,65 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
-Console.WriteLine("Введите число 1");
- int num1 = Convert.ToInt32(Console.ReadLine());
+// int [] array = new int [4];
+// int count=0;
+//     for(int i = 0; i<array.Length; i++)
+//     { 
+//         array[i]=new Random().Next(100,1000);
+//         Console.Write(array[i]+",");
+//         array[i]%=2;
+//     if (array[i]==0)
+//     {
+//       count++;
+//     }
+//         }
+        
+//         Console.WriteLine("Сумма четных чисел= "+ count);
+       
 
-Console.WriteLine("Введите число 2");
- int num2 = Convert.ToInt32(Console.ReadLine());
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
-double num = Math.Pow(num1,num2);
-Console.WriteLine(num);
+// int [] array = new int [5];
+// int sum=0;
+//     for(int i = 0; i<array.Length; i++)
+//     { 
+//         array[i]=new Random().Next(0,100);
+//         Console.Write(array[i]+", ");
+//         if (i%2 != 0)
+//         {
+//            sum=sum+array[i];
 
+//         }
+//     }
+        
+//         Console.WriteLine("Сумма элементов, стоящих на нечётных позициях= "+ sum);
 
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
 
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
+// int [] array = new int [5];
 
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
-int SumNum(int number)
-{
-    int sum = 0;
- 
-while (number>0)
-{
-    sum=sum+number%10;
-    number=number/10;
+//  for(int i = 0; i<array.Length; i++)
+//  { 
+//      array[i]=new Random().Next(-9,150);
+//       Console.Write(array[i]+",");
+//  }
+//  int max= array[0];
+//  int min= array[0];
+//  for (int i = 0; i < array.Length; i++)
+//  {
+//     if (array[i]>max)
+//     {
+//        max= array[i];
 
-}
-return sum;
-}
-Console.WriteLine("Сумма цифр равна " + SumNum(num));
-
-// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]
-
-int [] array = GetArray();
-int[] GetArray()
-{
-    int []array= new int [8];
-
-    for(int i =0; i<8;i++)
-    { 
-        array[i]=new Random().Next(1,1000);
-        }
-        return array;
-}
-Console.Write(String.Join(",",array));
+//     }
+//     if (array[i]<min)
+//     {
+//        min= array[i];
+       
+//     }
+//  }
+// Console.WriteLine($"разница между max&min равна {max-min}");
